15488dc138dd74349cd3ff6ccd80fd09b7024c35
yin1991 <84140478+xiaokuili@users.noreply.github.com>
2023-05-10 03:03:20 +0000

    创建system_info和game对象

---
+++ checkers/x/checkers/types/errors.go
@@ -0,0 +1,15 @@
+package types
+
+// DONTCOVER
+
+import (
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+
+
+var (
+    ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
+    ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+    ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
+)
